86bd99ddadcbaf080d156c727a0b6169cdf76643
Namrata-Kadasi <namratakadasi3101@gmail.com>
2022-01-18 18:20:17 +0530

    Database initialisation and setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,15 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 dotenv.config();
 const app=express();
+mongoose.connect(process.env.MONGODB_URL)
+.then(function(){
+  console.log("Connected to Mongo DB database");
+})
+.catch(function(err){
+  console.log("Error connecting to database"+err);
+});
 app.listen(process.env.PORT,function(){
   console.log("Server has started at port "+process.env.PORT);
 });
